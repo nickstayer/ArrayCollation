d2d66f388e5e810d7233f4fa57d200d0b99ec340
nickstayer <dubovskov@yandex.ru>
2024-08-28 23:01:27 +0500

    first commit again

---
+++ ArrayCollation/Import.cs
@@ -0,0 +1,5 @@
+﻿global using System.Windows;
+global using System.IO;
+global using System.Text;
+global using CreateTable;
+global using ExceLib;
---
+++ ArrayCollation/Resource.cs
@@ -0,0 +1,8 @@
+﻿namespace ArrayCollation;
+
+public class Resource
+{
+    public required string Name { get; set; }
+    public required string TableName { get; set; }
+    public required string[] TableColumns { get; set; }
+}
