b2cdd058ea10d57c9289bfed7d22d414ddab1b04
nickstayer <dubovskov@yandex.ru>
2024-08-28 22:59:46 +0500

    Remove ignored files from repository

--- ArrayCollation/Import.cs
+++
@@ -1,5 +0,0 @@
-﻿global using System.Windows;
-global using System.IO;
-global using System.Text;
-global using CreateTable;
-global using ExceLib;
--- ArrayCollation/Resource.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ArrayCollation;
-
-public class Resource
-{
-    public required string Name { get; set; }
-    public required string TableName { get; set; }
-    public required string[] TableColumns { get; set; }
-}
